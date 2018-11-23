d44405d30c4469b3d5d62656b3769dd5e27bbe51
Flowmaster <ellyssin.gimhae@gmail.com>
2018-11-22 21:45:03 -0700

    Added socket component.

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,9 @@ const Header = () => (
         <Link href="/about">
           <a style={linkStyle}>About</a>
         </Link>
+        <Link href="/socket">
+          <a style={linkStyle}>Socket</a>
+        </Link>
     </div>
 )
 
