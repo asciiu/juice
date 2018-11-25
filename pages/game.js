04f06e7b510e5349055abf1397dae522d8838114
Flowmaster <ellyssin.gimhae@gmail.com>
2018-11-24 22:46:18 -0700

    Removed react-game-kit and socket.io deps. Use script-loader to load create.js and tween.js from the scripts folder.

--- pages/game.js
+++ pages/game.js
@@ -1,5 +1,6 @@
-import { Loop, Stage } from 'react-game-kit';
 import Layout from '../components/MyLayout.js'
+import 'script-loader!../scripts/createjs.min.js';
+import 'script-loader!../scripts/tweenjs.min.js';
 
 class Game extends React.Component {
   constructor(props) {
@@ -9,20 +10,40 @@ class Game extends React.Component {
     //this.handleClick = this.handleClick.bind(this);
   }
 
+  componentDidMount() {
+    //document.head.appendChild(script);
+    this.init()
+  }
+
+  init() {
+    let stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+
+    createjs.Tween.get(circle, { loop: true })
+    .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+    .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ alpha: 0, y: 225 }, 100)
+    .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", stage);
+  }
+
   render() {
     return (
-      <Loop>
-        <Stage>
-          // Game specific components go here
-        </Stage>
-      </Loop>
+      <Layout>
+        <p>androidsex</p>
+        <p> coming soon! </p>
+        <canvas id="demoCanvas" width="500" height="300"></canvas>
+      </Layout>
     );
   }
 }
 
 export default () => (
-  <Layout>
-    <p>This is a game!</p>
     <Game />
-  </Layout>
 )
