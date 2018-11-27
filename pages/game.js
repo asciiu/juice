e9ce92d581b0534163279bf245f64c396ca65193
Flowmaster <ellyssin.gimhae@gmail.com>
2018-11-26 19:51:30 -0700

    Register window onkeydown to capture key events. Prevent default in key handler so it does not scroll the window.

--- pages/game.js
+++ pages/game.js
@@ -1,23 +1,25 @@
 import Layout from '../components/MyLayout.js'
 import 'script-loader!../scripts/createjs.min.js';
-import 'script-loader!../scripts/tweenjs.min.js';
-import 'script-loader!../scripts/soundjs.min.js';
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     //this.state = {messages: []};
-    // This binding is necessary to make `this` work in the callback
-    this.playSound = this.playSound.bind(this);
   }
 
   componentDidMount() {
+    window.onkeydown = this.handleKeyDown;
     this.init()
   }
 
-  playSound () {
+  componentWillUnmount() {
+    // need to unregister the keydown handler otherwise it will remain
+    // unregistered after unmount
+    window.onkeydown = null;
+  }
+
+  playSound = (e) => {
     if (this.soundID) {
-      console.log(this.soundID);
       createjs.Sound.play(this.soundID);
     }
   }
@@ -30,25 +32,54 @@ class Game extends React.Component {
     circle.y = 100;
     stage.addChild(circle);
 
-    createjs.Tween.get(circle, { loop: true })
-    .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-    .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-    .to({ alpha: 0, y: 225 }, 100)
-    .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-    .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+    this.circle = circle;
+    this.stage = stage;
     createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", stage);
+    createjs.Ticker.addEventListener("tick", this.handleTick);
 
     this.soundID = "thunder";
     createjs.Sound.registerSound("/static/squeakyToy.mp3", this.soundID);
+  }
+
+  handleTick = (e) => {
+    //Will cause the circle to wrap back
+    if (this.circle.x > this.stage.canvas.width) { this.circle.x = 0; }
+      this.stage.update();
+
+    // Actions carried out each tick (aka frame)
+    if (!e.paused) {
+       // Actions carried out when the Ticker is not paused.
+    } 
+  }
+
+  handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    
+    switch(e.keyCode) {
+      case 37:
+        this.circle.x -= 3;
+        break;
+      case 38:
+        this.circle.y -= 3;
+        break;
+      case 39:
+        this.circle.x += 3;
+        break;
+      case 40:
+        this.circle.y += 3;
+        break;
+      default:
+        return;
+    }
+    e.preventDefault();
   }
 
   render() {
     return (
       <Layout>
         <p>ANEX coming soon!</p>
-        <canvas id="demoCanvas" width="500" height="300"></canvas>
         <button onClick={this.playSound}>Play Sound</button>
+        <canvas id="demoCanvas" width="1024" height="500" />
       </Layout>
     );
   }
