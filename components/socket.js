2d3872fe736a6e0a722ec94171ffc75e6e92fa1f
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-11 13:10:26 -0700

    Removed the socket message buffer. Socket messages should be sent immediately.

--- components/socket.js
+++ components/socket.js
@@ -1,7 +1,6 @@
 export class GameSocket {
   constructor(url) {
     this.url = url;
-    this.messages = [];
     this.connection = null;
   }
 
@@ -12,34 +11,15 @@ export class GameSocket {
     this.connection.onmessage = onMessage; 
     this.connection.onopen = onOpen;
     this.connection.onclose = onClose;
-    this.t = setInterval(this.flush, 30);
   }
 
   close = () => {
-    clearInterval(this.t);
     this.connection.close();
   }
 
-  flush = () => {
-    if (this.messages.length > 0 && this.connection.readyState == this.connection.OPEN) {
-      this.connection.send(JSON.stringify(this.messages));
-      this.messages = [];
-    }
-  }
 
   // json is expected to be an array of json
-  send = (jsonarray) => {
-    for (const json of jsonarray) {
-      const msg = this.messages.find(m => 
-        m.clientID == json.clientID && m.topic == json.topic);
-      
-      if (msg == undefined) {
-        // add new message
-        this.messages.push(json);
-      } else {
-        // overwrite existing
-        msg = json;
-      }
-    }
+  send = (messages) => {
+    this.connection.send(JSON.stringify(messages));
   }
 }
