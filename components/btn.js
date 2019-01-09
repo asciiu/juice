db7ccc83c10b9bba2d7fa6f3ee9bc9c137c50258
Flowmaster <ellyssin.gimhae@gmail.com>
2019-01-08 18:58:49 -0700

    Added styled-jsx for button component.

--- components/btn.js
+++ components/btn.js
@@ -2,12 +2,25 @@ import React from 'react'
 import {withRouter} from 'next/router'
 
 const Btn = ({href, onClick, children, router}) => (
-    <span>
-        <button onClick={onClick} 
-                style={{fontWeight : router.pathname == href ? 'bold' : ''}}>
-                {children}
-        </button>
-    </span>
+  <span>
+    <button onClick={onClick} 
+      style={{fontWeight : router.pathname == href ? 'current' : ''}}>
+      {children}
+    </button>
+    <style jsx>{`
+      button {
+        color: blue;
+        border: 1px solid;
+        cursor: pointer;
+      }
+      button:hover {
+        color: red;
+      }
+      button.current {
+        font-weight: bold;
+      }
+    }`}</style>
+  </span>
 )
 
 export default withRouter(Btn)
