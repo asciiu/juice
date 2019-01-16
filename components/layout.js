8e715388acd08ecf5d634a7c7d24100237d6901b
Flowmaster <ellyssin.gimhae@gmail.com>
2019-01-15 23:09:29 -0700

    Inline the background color for the login menu item since the jsx style did not work.

--- components/layout.js
+++ components/layout.js
@@ -35,7 +35,7 @@ export default class JuiceLayout extends React.Component{
     const { loading } = this.state;
 
     return (
-      <nav>
+      <span>
         <Modal
             title="Login"
             visible={this.state.visible}
@@ -56,13 +56,12 @@ export default class JuiceLayout extends React.Component{
               mode="horizontal"
               defaultSelectedKeys={['1']}
               style={{ lineHeight: '64px' }}
-              onClick={this.onNav}
             >
               <Menu.Item key="index" style={{float: 'left'}}><NavLink href="/" passHref>yuice</NavLink></Menu.Item>
               <Menu.Item key="bet" style={{float: 'left'}}><NavLink href="/bet" passHref>bet</NavLink></Menu.Item>
               <Menu.Item key="about" style={{float: 'left'}}><NavLink href="/about" passHref>about</NavLink></Menu.Item>
               <Menu.Item key="signup" style={{float: 'right'}}><NavLink href="/signup" passHref>signup</NavLink></Menu.Item>
-              <Menu.Item key="login" style={{float: 'right'}}> 
+              <Menu.Item key="login" style={{float: 'right', background: '#001529'}}>
                 <Button type="primary" onClick={this.showModal}>
                   login
                 </Button>
@@ -76,12 +75,7 @@ export default class JuiceLayout extends React.Component{
             yuice ©2019 Created by Axl Codes 
           </Footer>
         </Layout>
-        <style jsx>{`
-          .ant-menu-item-selected {
-            background: #001529;
-          }
-        `}</style>
-      </nav>
+      </span>
     )
   }
 }
