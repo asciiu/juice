3f5ad8e1aa98834b9f7567645abc6e6de469ce1f
Flowmaster <ellyssin.gimhae@gmail.com>
2019-01-17 22:03:41 -0700

    Added not about simple bet.

--- pages/bet.js
+++ pages/bet.js
@@ -5,7 +5,13 @@ import SimpleBetForm from '../components/forms/simpleBetForm'
 export default () => (
   <Layout>
     <Row>
-      <Col span={12} offset={6}>
+      <Col span={6} >
+        <p>This is a staight bet. Your opponent needs to bet the same amount as you.</p> 
+        <p>An example of this type of bet would be when you want to bet your friend 
+          0.0003 BTC if the Detroit Lions make it to the super bowl this year. There is 
+          no time limit as both of you will determine when the bet ends.</p>
+      </Col>
+      <Col span={12} offset={0}>
         <SimpleBetForm />
       </Col>
     </Row>
